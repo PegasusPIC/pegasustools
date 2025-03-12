ab4658e1410fcdb910d7316a846e73449bfe2168
Bob Caddy <rcaddy@princeton.edu>
2025-03-12 12:14:53 -0400

    Initial Commit. Created from Cookie at v2025.01.22
    This is the result of running `copier` to instantiate the project
    template from the [cookie](https://github.com/scientific-python/cookie)
    template.

---
+++ src/pegasustools/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2025 Robert Caddy. All rights reserved.
+
+pegasustools: Analysis package for the Pegasus PIC code.
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
