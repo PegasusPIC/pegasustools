d0f2eef530898b82a7e72ffe036ff75feb40c51a
Bob Caddy <rcaddy@princeton.edu>
2025-03-14 14:14:12 -0400

    feat: add some dummy functions to prove packaging works

--- src/pegasustools/__init__.py
+++ src/pegasustools/__init__.py
@@ -6,4 +6,8 @@ pegasustools: Analysis package for the Pegasus PIC code.
 
 from ._version import version as __version__
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "times_2", "times_6"]
+
+# Import all relevant modules into the pegasustools namespace
+from pegasustools.file1 import times_2
+from pegasustools.file2 import times_6
---
+++ src/pegasustools/file2.py
@@ -0,0 +1,5 @@
+import pegasustools as pt
+
+
+def times_6(x: int) -> int:
+    return 3 * pt.times_2(x)
