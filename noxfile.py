e3f2262b2286dedfdc63b902b642b5578fe055d4
Bob Caddy <rcaddy@princeton.edu>
2025-03-20 14:03:00 -0400

    doc: add templates for apidocs

--- noxfile.py
+++ noxfile.py
@@ -87,6 +87,7 @@ def build_api_docs(session: nox.Session) -> None:
         "--module-first",
         "--no-toc",
         "--force",
+        "--templatedir=./docs/_templates/apidoc",
         "src/pegasustools",
     )
 
