fc8917ca0c1c0d013f413baa31ec1a66480a8411
Bob Caddy <rcaddy@princeton.edu>
2025-04-08 15:19:48 -0400

    feat: add support for loading spectra files.
    Added the `PegasusSpectralData` class to load data from spectra files.
    Also added documentation and tests for `PegasusSpectralData`.

--- src/pegasustools/__init__.py
+++ src/pegasustools/__init__.py
@@ -1,10 +1,11 @@
 """Copyright (c) 2025 Robert Caddy. All rights reserved.
 
-pegasustools: Analysis package for the Pegasus PIC code.
+PegasusTools: Analysis package for the Pegasus PIC code.
 """
 
 # Import all relevant modules into the pegasustools namespacess
 from pegasustools.loading_nbf import PegasusNBFData
+from pegasustools.loading_spectra import PegasusSpectralData
 
 # Set version
 from ._version import version as __version__
@@ -12,5 +13,6 @@ from ._version import version as __version__
 # Setup what gets imported with import *
 __all__ = [
     "PegasusNBFData",
+    "PegasusSpectralData",
     "__version__",
 ]
