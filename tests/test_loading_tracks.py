9bf6f601c57b2392c4f1b2425b5f6d4de31849be
Bob Caddy <rcaddy@princeton.edu>
2025-04-23 13:20:09 -0400

    feat: Add tests

--- tests/test_loading_tracks.py
+++ tests/test_loading_tracks.py
@@ -1,8 +1,10 @@
 """Tests for the contents of loading_tracks.py."""
 
+import re
 from pathlib import Path
 
 import numpy as np
+import pytest
 
 import pegasustools as pt
 
@@ -33,6 +35,75 @@ def test_PegasusTrack_ASCII() -> None:
             test.data[field].astype(np.float32),
             maxulp=10,
         )
+
+
+def test_PegasusTrack_ASCII_not_pegasus_file() -> None:
+    """Test for the exception that should appear if the file has the wrong header."""
+    # Setup paths
+    file_path = Path(__file__).parent.resolve() / "data" / "wrong_header.track.dat"
+    with file_path.open("w") as file:
+        file.write("this is not the correct header\n")
+
+    err_msg = f"The file at {file_path} is not a Pegasus++ .track.dat file."
+    with pytest.raises(RuntimeError, match=re.escape(err_msg)):
+        pt.PegasusTrack(file_path)
+
+
+def test_PegasusTrack_wrong_extension() -> None:
+    """Test for the exception that should appear if the file has the wrong extension."""
+    # Setup paths
+    file_path = Path(__file__).parent.resolve() / "data" / "wrong_extension.txt"
+    with file_path.open("w") as file:
+        file.write("this is placeholder text\n")
+
+    err_msg = f"The file at {file_path} is not a Pegasus++ tracked particle file."
+    with pytest.raises(RuntimeError, match=re.escape(err_msg)):
+        pt.PegasusTrack(file_path)
+
+
+def test_PegasusTrack_bad_ids() -> None:
+    """Test for exception when missing or bad arguments.
+
+    Test for the exception that should appear when trying to load the collated HDF5 file
+    without block or particle IDs or if those IDs are negative
+    """
+    # Setup paths
+    file_path = Path(__file__).parent.resolve() / "data" / "wrong_ids.hdf5"
+    with file_path.open("w") as file:
+        file.write("this is placeholder text\n")
+
+    err_msg = (
+        "block_id and particle_id are required when loading a dataset from "
+        "a collated HDF5 file and they both must be positive ints."
+    )
+
+    # ===== Tests for missing args =====
+
+    # Missing both
+    with pytest.raises(ValueError, match=re.escape(err_msg)):
+        pt.PegasusTrack(file_path)
+
+    # Missing block_id
+    with pytest.raises(ValueError, match=re.escape(err_msg)):
+        pt.PegasusTrack(file_path, block_id=10)
+
+    # Missing particle_id
+    with pytest.raises(ValueError, match=re.escape(err_msg)):
+        pt.PegasusTrack(file_path, particle_id=10)
+
+    # ===== Tests for negative args =====
+
+    # Both negative
+    with pytest.raises(ValueError, match=re.escape(err_msg)):
+        pt.PegasusTrack(file_path, block_id=-10, particle_id=-10)
+
+    # block_id negative
+    with pytest.raises(ValueError, match=re.escape(err_msg)):
+        pt.PegasusTrack(file_path, block_id=-10)
+
+    # particle_id negative
+    with pytest.raises(ValueError, match=re.escape(err_msg)):
+        pt.PegasusTrack(file_path, particle_id=-10)
 
 
 def generate_random_track_ascii(
