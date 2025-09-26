08d6b5eca9e99fcad297c28a0cb2015d354a8edb
Bob Caddy <rcaddy@princeton.edu>
2025-09-26 11:57:24 -0400

    feat: partial support for multiple ion spectra files

--- src/pegasustools/loading_spectra.py
+++ src/pegasustools/loading_spectra.py
@@ -5,6 +5,7 @@ This module provides:
 - _load_spectra: A function for loading spectra files
 """
 
+import typing
 from pathlib import Path
 
 import numpy as np
@@ -56,80 +57,119 @@ class PegasusSpectralData:
             provided n_prl and n_prp
         """
         # Define member variables
-        self.__n_prp: int = n_prp
-        self.__n_prl: int = n_prl
-        self.__max_w_prp: float = max_w_prp
-        self.__max_w_prl: float = max_w_prl
+        self.__time: np.float64 = np.nan
+        self.__num_ions: int = 1
+        self.__n_prp: int | tuple[int, ...] = n_prp
+        self.__n_prl: int | tuple[int, ...] = n_prl
+        self.__max_w_prp: float | tuple[float, ...] = max_w_prp
+        self.__max_w_prl: float | tuple[float, ...] = max_w_prl
         self.spectra_prp: np.typing.NDArray[np.float64]
         self.spectra_prl: np.typing.NDArray[np.float64]
 
         # Open the file
         with file_path.open(mode="rb") as spec_file:
             # Load the header
-            header = spec_file.readline().decode("ascii")
-            second_line_loc = spec_file.tell()
-            self.__time: np.float64 = np.float64(header.split()[-1])
-
-            # Check if this is the new spectra format with the complete header
-            n_prl_line_bytes = spec_file.readline()
-
-            # Check if line_2 is binary or part of the header
-            is_ascii = True
-            try:
-                n_prl_line = n_prl_line_bytes.decode("ascii")
-            except UnicodeDecodeError:
-                is_ascii = False
-
-            if is_ascii and "Histogram size in wprl = " in n_prl_line:
-                # Now we need to decode the contents of the new header
-                self.__n_prl = int(n_prl_line.rstrip().split(" ")[-1])
-                self.__n_prp = int(
-                    spec_file.readline().decode("ascii").rstrip().split(" ")[-1]
-                )
-                self.__max_w_prl = float(
-                    spec_file.readline().decode("ascii").rstrip().split(" ")[-1]
-                )
-                self.__max_w_prp = float(
-                    spec_file.readline().decode("ascii").rstrip().split(" ")[-1]
-                )
-            else:
-                spec_file.seek(second_line_loc)
+            self.__read_header(spec_file)
 
             # Load the entire remaining file
             self.data = np.fromfile(spec_file, dtype=np.float64)
 
-            # Get the info to reshape the array
-            block_header_size = 6  # The header of each block is 6 elements
-            num_row = self.__n_prl * self.__n_prp + block_header_size
-            num_col = self.data.size // num_row
+        # Reshape the loaded data to extract the header and spectra in the correct shape
+        if self.__num_ions == 1:
+            self.__reshape_single_ion(file_path)
+        else:
+            self.__reshape_multiple_ion(file_path)
 
-            # Check that the file is actually the right size for the number of elements
-            # per spectra. Note that this check isn't perfect, it just verifies that the
-            # file can be exactly divided by the number of elements provided.
-            if self.data.size % num_row != 0:
-                err_msg = (
-                    f"The file {file_path} does not have the right number of "
-                    f"elements for the values of {self.__n_prl = } and "
-                    f"{self.__n_prp = } provided."
-                )
-                raise ValueError(err_msg)
+    def __read_header(self, spec_file: typing.BinaryIO) -> None:
+        # Read in each line one by one until one of them isn't ASCII
+        header = []
+        while True:
+            line_start = spec_file.tell()
+            line = spec_file.readline()
+            # Check if the line is ASCII or binary
+            try:
+                # Decode line to ascii and remove newline
+                header.append(line.decode("ascii").rstrip())
+            except UnicodeDecodeError:
+                # Reset file to the beginning of this line
+                spec_file.seek(line_start)
+                break
 
-            # Rearrange the data into the correct shape including the header for each
-            # meshblock
-            self.data = self.data.reshape((num_col, num_row))
+        # Get time
+        self.__time = float(header.pop(0).split()[-1])
 
-            # Slice off the header and main data
-            headers = self.data[:, :block_header_size]
-            self.data = self.data[:, block_header_size:]
+        # Check for the old format
+        if len(header) == 0:
+            # This is the old header, it only contains the time
+            return
 
-            # Reshape data now that the headers have been removed
-            self.data = self.data.reshape((num_col, self.__n_prp, self.__n_prl))
+        # Get the number of ion species if it's there
+        if "Number of minor-ion species =" in header[0]:
+            self.__num_ions = int(header.pop(0).split()[-1])
 
-            # Organize the meshblock headers into a DataFrame
-            self._meshblock_locations = pl.from_numpy(
-                headers,
-                schema=("x1min", "x1max", "x2min", "x2max", "x3min", "x3max"),
+        # Select path based on the number of ions
+        if self.__num_ions == 1:
+            # This is a new header with a single ion
+            self.__n_prl = int(header.pop(0).split()[-1])
+            self.__n_prp = int(header.pop(0).split()[-1])
+            self.__max_w_prl = float(header.pop(0).split()[-1])
+            self.__max_w_prp = float(header.pop(0).split()[-1])
+        else:
+            # This is a new header with multiple ions
+            # Loop through the header to read the data for each ion
+            n_prl, n_prp, max_w_prl, max_w_prp = [], [], [], []
+            for _ in range(self.__num_ions):
+                n_prl.append(int(header.pop(0).split()[-1]))
+                n_prp.append(int(header.pop(0).split()[-1]))
+                max_w_prl.append(float(header.pop(0).split()[-1]))
+                max_w_prp.append(float(header.pop(0).split()[-1]))
+
+            # Copy the mutable lists to immutable tuples in member variables so they
+            # can't be modified later
+            self.__n_prl = tuple(n_prl)
+            self.__n_prp = tuple(n_prp)
+            self.__max_w_prl = tuple(max_w_prl)
+            self.__max_w_prp = tuple(max_w_prp)
+
+    def __reshape_single_ion(self, file_path: Path) -> None:
+        assert isinstance(self.__n_prl, int)  # noqa: S101
+        assert isinstance(self.__n_prp, int)  # noqa: S101
+
+        # Get the info to reshape the array
+        block_header_size = 6  # The header of each block is 6 elements
+        num_row = self.__n_prl * self.__n_prp + block_header_size
+        num_col = self.data.size // num_row
+
+        # Check that the file is actually the right size for the number of elements
+        # per spectra. Note that this check isn't perfect, it just verifies that the
+        # file can be exactly divided by the number of elements provided.
+        if self.data.size % num_row != 0:
+            err_msg = (
+                f"The file {file_path} does not have the right number of "
+                f"elements for the values of {self.__n_prl = } and "
+                f"{self.__n_prp = } provided."
             )
+            raise ValueError(err_msg)
+
+        # Rearrange the data into the correct shape including the header for each
+        # meshblock
+        self.data = self.data.reshape((num_col, num_row))
+
+        # Slice off the header and main data
+        headers = self.data[:, :block_header_size]
+        self.data = self.data[:, block_header_size:]
+
+        # Reshape data now that the headers have been removed
+        self.data = self.data.reshape((num_col, self.__n_prp, self.__n_prl))
+
+        # Organize the meshblock headers into a DataFrame
+        self._meshblock_locations = pl.from_numpy(
+            headers,
+            schema=("x1min", "x1max", "x2min", "x2max", "x3min", "x3max"),
+        )
+
+    def __reshape_multiple_ion(self, file_path: Path) -> None:
+        pass
 
     # Define getters for header variables
     @property
@@ -144,46 +184,57 @@ class PegasusSpectralData:
         return self.__time
 
     @property
-    def n_prp(self) -> int:
+    def num_ions(self) -> int:
+        """Get the number of ions in this file.
+
+        Returns
+        -------
+        int
+            The number of ions in the spectra file
+        """
+        return self.__num_ions
+
+    @property
+    def n_prp(self) -> int | tuple[int, ...]:
         """Get n_prp.
 
         Returns
         -------
-        int
-            The value of n_prp
+        int | tuple[int, ...]
+            The value(s) of n_prp
         """
         return self.__n_prp
 
     @property
-    def n_prl(self) -> int:
+    def n_prl(self) -> int | tuple[int, ...]:
         """Get n_prl.
 
         Returns
         -------
-        int
-            The value of n_prl
+        int | tuple[int, ...]
+            The value(s) of n_prl
         """
         return self.__n_prl
 
     @property
-    def max_w_prp(self) -> float:
+    def max_w_prp(self) -> float | tuple[float, ...]:
         """Get max_w_prp.
 
         Returns
         -------
-        int
-            The value of max_w_prp
+        float | tuple[float, ...]
+            The value(s) of max_w_prp
         """
         return self.__max_w_prp
 
     @property
-    def max_w_prl(self) -> float:
+    def max_w_prl(self) -> float | tuple[float, ...]:
         """Get max_w_prl.
 
         Returns
         -------
-        int
-            The value of max_w_prl
+        float | tuple[float, ...]
+            The value(s) of max_w_prl
         """
         return self.__max_w_prl
 
