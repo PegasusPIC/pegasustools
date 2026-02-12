231b4a2c238df70f9a2e203737b1a2eba0c404cf
Bob Caddy <rcaddy@princeton.edu>
2026-02-12 13:56:16 -0500

    refactor: Move most of the code to load spec files into a new method
    This refactor means that we can have one function for .spec files and
    another for .specav style files.
    
    I also simplified the header loading significantly

--- src/pegasustools/loading_spectra.py
+++ src/pegasustools/loading_spectra.py
@@ -66,79 +66,97 @@ class PegasusSpectralData:
         # Open the file
         with file_path.open(mode="rb") as spec_file:
             # Load the header
-            header = spec_file.readline().decode("ascii")
-            second_line_loc = spec_file.tell()
-            self.__time: np.float64 = np.float64(header.split()[-1])
-
-            # Check if this is the new spectra format with the complete header
-            n_prl_line_bytes = spec_file.readline()
-
-            # Check if line_2 is binary or part of the header
-            is_ascii = True
-            try:
-                n_prl_line = n_prl_line_bytes.decode("ascii")
-            except UnicodeDecodeError:
-                is_ascii = False
-
-            if is_ascii and "Histogram size in wprl = " in n_prl_line:
-                # Now we need to decode the contents of the new header
-                self.__n_prl = int(n_prl_line.rstrip().split(" ")[-1])
-                self.__n_prp = int(
-                    spec_file.readline().decode("ascii").rstrip().split(" ")[-1]
-                )
-                self.__max_w_prl = float(
-                    spec_file.readline().decode("ascii").rstrip().split(" ")[-1]
-                )
-                self.__max_w_prp = float(
-                    spec_file.readline().decode("ascii").rstrip().split(" ")[-1]
-                )
-            else:
-                spec_file.seek(second_line_loc)
+            ascii_header: list[str] = []
+            while True:
+                reset_location = spec_file.tell()
+                try:
+                    ascii_header.append(spec_file.readline().decode("ascii").rstrip())
+                except UnicodeDecodeError:
+                    spec_file.seek(reset_location)
+                    break
 
             # Load the entire remaining file
             self.data = np.fromfile(spec_file, dtype=np.float64)
 
-            # Get the info to reshape the array
-            block_header_size = 6  # The header of each block is 6 elements
-            num_row = self.__n_prl * self.__n_prp + block_header_size
-            num_col = self.data.size // num_row
+        # Now that we have the data and the header we'll pass it off to routines for
+        # spec files and for specav style files
+        if file_path.suffix == ".spec":
+            self.__process_spec_files(file_path, ascii_header)
+        elif file_path.suffix in [None]:
+            pass
+        else:
+            msg = f"Error: {file_path} is not a spectra file."
+            raise RuntimeError(msg)
 
-            # Check that the file is actually the right size for the number of elements
-            # per spectra. Note that this check isn't perfect, it just verifies that the
-            # file can be exactly divided by the number of elements provided.
-            if self.data.size % num_row != 0:
-                err_msg = (
-                    f"The file {file_path} does not have the right number of "
-                    f"elements for the values of {self.__n_prl = } and "
-                    f"{self.__n_prp = } provided."
-                )
-                raise ValueError(err_msg)
+    def __process_spec_files(self, file_path: Path, ascii_header: list[str]) -> None:
+        """Process .spec files.
 
-            # Rearrange the data into the correct shape including the header for each
-            # meshblock
-            self.data = self.data.reshape((num_col, num_row))
+        Parameters
+        ----------
+        file_path : Path
+            The path to the spec file
+        ascii_header : list[str]
+            The ascii header to the file.
 
-            # Slice off the header and main data
-            headers = self.data[:, :block_header_size]
-            self.data = self.data[:, block_header_size:]
+        Raises
+        ------
+        ValueError
+            Thrown if the dataset is not the right size for the provided n_prp and n_prl
+        """
+        self.__time = np.float32(ascii_header[0].split()[-1])
 
-            # Reshape data now that the headers have been removed
-            self.data = self.data.reshape((num_col, self.__n_prp, self.__n_prl))
+        # Check if this is the new spectra format with the complete header
+        if len(ascii_header) > 1:
+            # This is the new header type
+            self.__n_prl = int(ascii_header[1].split(" ")[-1])
+            self.__n_prp = int(ascii_header[2].split(" ")[-1])
+            self.__max_w_prl = float(ascii_header[3].split(" ")[-1])
+            self.__max_w_prp = float(ascii_header[4].split(" ")[-1])
 
-            # Organize the meshblock headers into a DataFrame
-            self._meshblock_locations = pl.from_numpy(
-                headers,
-                schema=("x1min", "x1max", "x2min", "x2max", "x3min", "x3max"),
+        # Get the info to reshape the array
+        block_header_size = 6  # The header of each block is 6 elements
+        num_row = self.__n_prl * self.__n_prp + block_header_size
+        num_col = self.data.size // num_row
+
+        # Check that the file is actually the right size for the number of elements
+        # per spectra. Note that this check isn't perfect, it just verifies that the
+        # file can be exactly divided by the number of elements provided.
+        if self.data.size % num_row != 0:
+            err_msg = (
+                f"The file {file_path} does not have the right number of "
+                f"elements for the values of {self.__n_prl = } and "
+                f"{self.__n_prp = } provided."
             )
+            raise ValueError(err_msg)
+
+        # Rearrange the data into the correct shape including the header for each
+        # meshblock
+        self.data = self.data.reshape((num_col, num_row))
+
+        # Slice off the header and main data
+        headers = self.data[:, :block_header_size]
+        self.data = self.data[:, block_header_size:]
+
+        # Reshape data now that the headers have been removed
+        self.data = self.data.reshape((num_col, self.__n_prp, self.__n_prl))
+
+        # Organize the meshblock headers into a DataFrame
+        self._meshblock_locations = pl.from_numpy(
+            headers,
+            schema=("x1min", "x1max", "x2min", "x2max", "x3min", "x3max"),
+        )
+
+    # def __process_specav_type_files(self) -> None:
+    #     pass
 
     # Define getters for header variables
     @property
-    def time(self) -> np.float64:
+    def time(self) -> np.float32:
         """Get the simulation time of the spectra file.
 
         Returns
         -------
-        np.float64
+        np.float32
             The time in the spectra file
         """
         return self.__time
